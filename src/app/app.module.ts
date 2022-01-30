9daa92bc9db6fe13fd91a36eb9618deb6b71c187
alex <pustovit@rozetka.com.ua>
2022-01-30 19:39:45 +0200

    svg

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SvgAnimationComponent } from './svg-animation/svg-animation.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SvgAnimationComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/svg-animation/svg-animation.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-svg-animation',
+  templateUrl: './svg-animation.component.html',
+  styleUrls: ['./svg-animation.component.scss']
+})
+export class SvgAnimationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
